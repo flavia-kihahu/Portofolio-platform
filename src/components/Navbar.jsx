8b2e2ce8c0b551acbea9e8a5bebcb9be4331cd70
flavia-kihahu <kihahuflavia@gmail.com>
2026-08-06 10:22:11 +0300

    done with navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
-// import logo from '../assets/logo';
+import logo from '../assets/logo.png';
 
 export default function Navbar() {
+    const handleProjectsClick = () => {
+        const projects = document.getElementById('projects');
+        projects.scrollIntoView({ behavior: 'smooth' });
+    };
     return (
-        <nav className="w-full bg-white shadow-sm sticky top-0 z-50 border-b border-slate-200" data-testid="nav">
+        <nav className="navbar">
+            <div className="logo">
+                <img src={logo} alt="logo-image"/>
+                <h1>Maker</h1>
+            </div>
+            <button className="project-btn" onClick={handleProjectsClick}>See Projects</button>
         </nav>
     );
 }
